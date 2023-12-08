a58d366f4e2e8c181c3e2742cdc2b37618959ff8
Daniel Muñoz Arroyo <daniel.munoz.arroyo@estudiantat.upc.edu>
2023-12-08 18:38:51 +0100

    Main menu with buttons and visualization of rocks

--- Assets/Scripts/Weapons/Pistol.cs
+++ Assets/Scripts/Weapons/Pistol.cs
@@ -36,7 +36,7 @@ public class Pistol : MonoBehaviour
         //Compute position
         float xPos = center.position.x + Mathf.Cos(bulletAngle) * 29f;
         float zPos = center.position.z + Mathf.Sin(bulletAngle) * 29f;
-        Vector3 pos = new Vector3(xPos, transform.position.y + 1.3f, zPos);
+        Vector3 pos = new Vector3(xPos, transform.position.y + 1.1f, zPos);
 
         //compute orientation (will be needed)
 
